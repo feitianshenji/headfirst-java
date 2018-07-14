b1ed908e11916a2a784ca32c11abcd9f5046d51e
feitianshenji <40733073+feitianshenji@users.noreply.github.com>
2018-07-14 16:06:08 +0800

    seralization_intro

---
+++ ch14/DungeonTest.java
@@ -0,0 +1,39 @@
+//p469
+
+import java.io.*;
+
+
+class DungeonGame implements Serializable {
+    public int x = 3;
+    transient long y = 4;
+    private short z = 5;
+    int getX() {
+        return x;
+    }
+    long getY() {
+        return y;
+    }
+    short getZ() {
+        return z;
+    }
+}
+
+public class DungeonTest {
+    public static void main(String[] args) {
+        DungeonGame d = new DungeonGame();
+        System.out.println(d.getX() + d.getY() + d.getZ());
+        try {
+            FileOutputStream fos = new FileOutputStream("dg.ser"); /*序列化*/
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(d);
+            oos.close();
+            FileInputStream fis = new FileInputStream("dg.ser"); /*解序列化*/
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            d = (DungeonGame) ois.readObject();
+            ois.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(d.getX() + d.getY() + d.getZ());
+    }
+}
